16983e514c449a0bbb9b811e6bb5721976121363
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-08-21 16:27:04 -0400

    formatted

--- twoNumberSum.py
+++ twoNumberSum.py
@@ -32,19 +32,19 @@ def twoNumberSumSecond(array, targetSum):
     return []
 
 
-#O(nLog(n)) time
-#O(1) space
+# O(nLog(n)) time
+# O(1) space
 def twoNumerSumThird(array, targetSum):
     array.sort()
     left = 0
     right = len(array) - 1
     while left < right:
-      currentSum = array[left] + array[right]
-      if currentSum == targetSum:
-          return [array[left], array[right]]
-      elif currentSum < targetSum:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
             left += 1
-      elif currentSum > targetSum:
+        elif currentSum > targetSum:
             right -= 1
     return []
 
